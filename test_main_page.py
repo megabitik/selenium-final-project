4de021ebce054e0c61db92e2d6aec7c22e8c35ec
megabitik <megabitik@gmail.com>
2021-04-14 17:42:08 +0700

    Added is_element_present(self, how, what)

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,14 @@
 from pages.main_page import MainPage
+import pytest
 
 
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+@pytest.mark.skip(reason="Will test only if the button is visible by now")
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
